ddbd6dc99579231fef95cfa6b92c783eb5850fbf
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-09-24 12:42:17 +0800

    配置@别名

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    hello world
+    <button class="hoverShadow">登录</button>
+    <p class="hoverShadow">a11</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import instance from "@/utils/request.ts";
+
+async function getHome() {
+  const res = await instance.get("/home/index");
+  console.log(res);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+getHome();
+</script>
+
+<style scoped lang="less">
+@import "@/assets/styles/variables.less";
+@import "@/assets/styles/mixins";
+
+button {
+  background-color: @xtxColor;
+  .hoverShadow();
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p {
+  .hoverShadow();
 }
 </style>
