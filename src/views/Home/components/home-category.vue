49299334a72e91a72dad95da9b08bfc7cf0297d3
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-10-11 22:33:50 +0800

    feat:渲染分类弹层

--- src/views/Home/components/home-category.vue
+++ src/views/Home/components/home-category.vue
@@ -1,21 +1,53 @@
 <script lang="ts" setup name="HomeCategory">
 import useStore from "@/store";
+import { GoodsType } from "@/types/data";
+import { ref } from "vue";
 
 const { useCategoryStore } = useStore();
+
+const currentGoods = ref<GoodsType[]>([]);
+const getCurrentGoods = (goods: GoodsType[]) => {
+  currentGoods.value = goods;
+};
 </script>
 <template>
   <div class="home-category">
     <ul class="menu">
-      <li v-for="item in useCategoryStore.list" :key="item.id">
+      <li
+        @mouseenter="getCurrentGoods(item.goods)"
+        v-for="item in useCategoryStore.list"
+        :key="item.id"
+      >
         <RouterLink to="/">{{ item.name }}</RouterLink>
         <RouterLink
           to="/"
-          v-for="sub in item.children.slice(0, 2)"
+          v-for="sub in item.children?.slice(0, 2)"
           :key="sub.id"
           >{{ sub.name }}
         </RouterLink>
       </li>
     </ul>
+    <div class="layer">
+      <h4>
+        分类推荐
+        <small>根据您的购买或浏览记录推荐</small>
+      </h4>
+      <ul>
+        <li v-for="i in currentGoods" :key="i.id">
+          <RouterLink to="/">
+            <img :src="i.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ i.name }}</p>
+              <p class="desc ellipsis">{{ i.desc }}</p>
+              <p class="price">
+                <i>¥</i>
+                {{ i.price }}
+              </p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -45,6 +77,94 @@ const { useCategoryStore } = useStore();
           font-size: 16px;
         }
       }
+    }
+  }
+
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+
+          &:hover {
+            background: #e3f9f4;
+          }
+
+          img {
+            width: 95px;
+            height: 95px;
+          }
+
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+
+            .desc {
+              color: #999;
+            }
+
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
