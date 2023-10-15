f9e92cc7c6fa88f91925bda090fbade8bc4192a8
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-10-15 22:37:50 +0800

    feat:封装面板组件结构样式

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import HomeBanner from "./components/home-banner.vue";
 import HomeCategory from "./components/home-category.vue";
+import HomePanel from "@/views/Home/components/home-panel.vue";
 </script>
 
 <template>
@@ -11,6 +12,8 @@ import HomeCategory from "./components/home-category.vue";
         <HomeCategory />
         <!-- banner轮播图 -->
         <HomeBanner />
+        <!--        面板-->
+        <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱" />
       </div>
     </div>
   </div>
