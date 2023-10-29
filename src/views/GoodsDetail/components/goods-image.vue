71bcf4ad601c74518943178f92e0fa345edf1185
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-10-29 12:50:51 +0800

    feat:配置鼠标移入显示大图和遮罩，以及遮罩移动

--- src/views/GoodsDetail/components/goods-image.vue
+++ src/views/GoodsDetail/components/goods-image.vue
@@ -1,16 +1,32 @@
 <script lang="ts" setup name="GoodsImage">
 import { ref } from "vue";
+import { useMouseInElement } from "@vueuse/core";
 
 defineProps<{
   goodsImg: string[];
 }>();
 
 const active = ref(0);
+
+const target = ref(null);
+const { isOutside, elementX, elementY } = useMouseInElement(target);
+
+console.log(isOutside);
 </script>
 <template>
-  <div class="goods-image" v-if="goodsImg.length">
-    <div class="middle">
+  <div class="goods-image" v-if="goodsImg">
+    <div
+      class="large"
+      :style="[{ backgroundImage: `url(${goodsImg[active]})` }]"
+      v-show="!isOutside"
+    ></div>
+    <div class="middle" ref="target">
       <img :src="goodsImg[active]" alt="" />
+      <div
+        :style="{ top: elementY + 'px', left: elementX + 'px' }"
+        v-show="!isOutside"
+        class="layer"
+      ></div>
     </div>
     <ul class="small">
       <li
@@ -31,10 +47,29 @@ const active = ref(0);
   height: 400px;
   position: relative;
   display: flex;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, 0.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
