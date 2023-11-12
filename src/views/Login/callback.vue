f56ca27aeaaafd3211648325345acbef7d5bf42b
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-11-12 22:26:46 +0800

    配置callback的登录表单组件

--- src/views/Login/callback.vue
+++ src/views/Login/callback.vue
@@ -1,23 +1,40 @@
 <script lang="ts" setup name="LoginCallback">
 import LoginHeader from "./components/login-header.vue";
 import LoginFooter from "./components/login-footer.vue";
+import { ref } from "vue";
+import CallbackBind from "@/views/Login/components/callback-bind.vue";
+import CallbackPatch from "@/views/Login/components/callback-patch.vue";
+
+const hasAccount = ref(true);
 </script>
 
 <template>
   <LoginHeader></LoginHeader>
   <section class="container">
     <nav class="tab">
-      <a href="javascript:">
+      <a
+        href="javascript:"
+        :class="{ active: hasAccount }"
+        @click="hasAccount = true"
+      >
         <i class="iconfont icon-bind" />
         <span>已有小兔鲜账号，请绑定手机</span>
       </a>
-      <a href="javascript:">
+      <a
+        href="javascript:"
+        :class="{ active: !hasAccount }"
+        @click="hasAccount = false"
+      >
         <i class="iconfont icon-edit" />
         <span>没有小兔鲜账号，请完善资料</span>
       </a>
     </nav>
-    <div class="tab-content" v-if="true">绑定手机</div>
-    <div class="tab-content" v-else>完善资料</div>
+    <div class="tab-content" v-if="hasAccount">
+      <callback-bind />
+    </div>
+    <div class="tab-content" v-else>
+      <callback-patch />
+    </div>
   </section>
   <LoginFooter></LoginFooter>
 </template>
